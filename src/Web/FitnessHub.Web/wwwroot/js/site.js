9d6fe91c9df32df0530f047bb04acc99f9d64a98
Golbarg200 <pepi_krastev@abv.bg>
2020-11-23 23:57:17 +0200

    Started Templating

--- src/Web/FitnessHub.Web/wwwroot/js/site.js
+++ src/Web/FitnessHub.Web/wwwroot/js/site.js
@@ -2,26 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-sendMessage = form => {
-    try {
-        $.ajax({
-            type: 'POST',
-            url: form.action,
-            data: new FormData(form),
-            contentType: false,
-            processData: false,
-            success: function (res) {
-                $('#view-all').html(res.html);
-                $.notify('submitted successfully', { globalPosition: 'top center', className: 'success' });
-            },
-            error: function (err) {
-                console.log(err)
-            }
-        })
-        //to prevent default form submit event
-        return false;
-    } catch (ex) {
-        console.log(ex)
-    }
-}
